7aa59b03f8b1ad4e5bde0f8884b0fbcb965d6460
Khalid Awlaqi <77898059+Dev-KhalidAi@users.noreply.github.com>
2021-06-15 14:20:47 +0300

    Adding vides and quizzed frames

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
-var test = document
-  .getElementById("componenet")
-  .addEventListener("click", test);
+componenets = document.getElementsByClassName("componenet");
+var componenet = Array.prototype.slice.call(componenets);
 
-var test2 = document.querySelector("#classes-container");
+for (var i = 0; i < componenet.length; i++) {
+  componenet[i].addEventListener("click", slider);
+}
+
+var videos = document.getElementsByClassName("video");
+var video = Array.prototype.slice.call(videos);
+
+for (var i = 0; i < video.length; i++) {
+  video[i].addEventListener("click", fillVideo);
+}
+
+var videoPath = document.getElementById("video-frame");
+videoPath.style.display = "none";
+
+quizes = document.getElementsByClassName("quiz");
+var quiz = Array.prototype.slice.call(quizes);
+
+for (var i = 0; i < quiz.length; i++) {
+  quiz[i].addEventListener("click", fillQuiz);
+}
+
+var quizFrame = document.querySelector(".quiz-frame");
+// quizFrame.style.display = "none";
+
+// var videos = document
+//   .getElementById("video")
+//   .addEventListener("click", fillVideo);
+
+var classesContainer = document.querySelector("#classes-container");
 var bool = false;
 
-function test() {
+function slider(index) {
+  var parent = index.target.parentElement;
+  var child = parent.children[1];
   if (bool == false) {
-    test2.style.display = "none";
-    test2.style.transition = "display 300ms";
+    child.style.display = "none";
     bool = true;
   } else {
-    test2.style.display = "";
+    child.style.display = "";
     bool = false;
   }
+}
+
+function fillVideo(index) {
+  quizFrame.style.display = "none";
+  videoPath.style.display = "";
+  videoPath.src = "https://www.youtube.com/embed/TXfGj7CFbqs";
+}
+
+function fillQuiz() {
+  videoPath.style.display = "none";
+  quizFrame.style.display = "";
 }
